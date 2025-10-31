84b27e1e249e45e7d3c25236e7119adf3f5d1749
Ranjodh Singh <ranjodhsingh201199@gmail.com>
2025-10-31 12:49:18 -0600

    added sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://punjabropeaccess.com',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'monthly',
+  priority: 0.8,
+};
